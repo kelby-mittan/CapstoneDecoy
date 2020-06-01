f96c745e270b503a96107439dda56f53207919a4
Kelby Mittan <kelbymittan@pursuit.org>
2020-05-31 21:00:28 -0400

    got bezier path working

--- CapstonePractice/Views + Controllers/WavyView.swift
+++ CapstonePractice/Views + Controllers/WavyView.swift
@@ -12,43 +12,44 @@ class WavyView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         return nil // TODO
     }
-
+    
     override func draw(_ rect: CGRect) {
         // Fill the whole background with the darkest blue color
-        let white: UIColor = .secondarySystemBackground
-        white.set()
-//        UIColor(red: 0.329, green: 0.218, blue: 0.875, alpha: 1).set()
+//        let white: UIColor = .white
+//        white.set()
+//        white.withAlphaComponent(0.0)
+                UIColor(red: 0, green: 0, blue: 0, alpha: 0).set()
         let bg = UIBezierPath(rect: rect)
         bg.fill()
-
+        
         // Add the first sine wave filled with a very transparent white
         let top1: CGFloat = 17.0
         let wave1 = wavyPath(rect: CGRect(x: 0, y: top1, width: frame.width, height: frame.height - top1), periods: 1.15, amplitude: 9, start: 4)
         let clear: UIColor = .systemTeal
         clear.set()
-//        UIColor(white: 1, alpha: 0.8).set()
+        //        UIColor(white: 1, alpha: 0.8).set()
         wave1.fill()
         
         
-
+        
         // Add the second sine wave over the first
-//        let top2: CGFloat = 34.0
-//        let wave2 = wavyPath(rect: CGRect(x: 0, y: top2, width: frame.width, height: frame.height - top2), periods: 1.5, amplitude: 21, start: 0.9)
-//        UIColor(white: 1.0, alpha: 0.15).set()
-//        wave2.fill()
-
+        //        let top2: CGFloat = 34.0
+        //        let wave2 = wavyPath(rect: CGRect(x: 0, y: top2, width: frame.width, height: frame.height - top2), periods: 1.5, amplitude: 21, start: 0.9)
+        //        UIColor(white: 1.0, alpha: 0.15).set()
+        //        wave2.fill()
+        
         // Add the text
-//        let paraAttrs = NSMutableParagraphStyle()
-//        paraAttrs.alignment = .center
-//        let textRect = CGRect(x: 0, y: frame.maxY - 64, width: frame.width, height: 24)
-//        let textAttrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.9), NSAttributedString.Key.paragraphStyle: paraAttrs]
-//        ("New user? Register here." as NSString).draw(in: textRect, withAttributes: textAttrs)
+        //        let paraAttrs = NSMutableParagraphStyle()
+        //        paraAttrs.alignment = .center
+        //        let textRect = CGRect(x: 0, y: frame.maxY - 64, width: frame.width, height: 24)
+        //        let textAttrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.9), NSAttributedString.Key.paragraphStyle: paraAttrs]
+        //        ("New user? Register here." as NSString).draw(in: textRect, withAttributes: textAttrs)
     }
-
+    
     // This creates the desired sine wave bezier path
     // rect is the area to fill with the sine wave
     // periods is how may sine waves fit across the width of the frame
@@ -56,10 +57,10 @@ class WavyView: UIView {
     // start is an offset in wavelengths for the left side of the sine wave
     func wavyPath(rect: CGRect, periods: Double, amplitude: Double, start: Double) -> UIBezierPath {
         let path = UIBezierPath()
-
+        
         // start in the bottom left corner
         path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-
+        
         let radsPerPoint = Double(rect.width) / periods / 2.0 / Double.pi
         let radOffset = start * 2 * Double.pi
         let xOffset = Double(rect.minX)
@@ -69,21 +70,21 @@ class WavyView: UIView {
         for x in stride(from: 0, to: Double(rect.width), by: 6) {
             let rad = Double(x) / radsPerPoint + radOffset
             let y = sin(rad) * amplitude
-
+            
             path.addLine(to: CGPoint(x: x + xOffset, y: y + yOffset))
         }
-
+        
         // Add the last point on the sine wave at the right edge
         let rad = Double(rect.width) / radsPerPoint + radOffset
         let y = sin(rad) * amplitude
-
+        
         path.addLine(to: CGPoint(x: Double(rect.maxX), y: y + yOffset))
-
+        
         // Add line from the end of the sine wave to the bottom right corner
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         // Close the path
         path.close()
-
+        
         return path
     }
 }
@@ -97,34 +98,35 @@ class Wave: UIView {
     
     public lazy var wView: WavyView = {
         let wavy = WavyView()
-//        wavy.backgroundColor = .red
+                wavy.backgroundColor = .clear
         return wavy
     }()
     
     public lazy var wImageView: WavyImageView = {
         let wavy = WavyImageView(frame: CGRect(x: 0, y: 0, width: 502, height: 172))
-//        wavy.backgroundColor = .red
-//        wavy.image = UIImage(named: "nycSeaLevel1")
+        //        wavy.backgroundColor = .red
+        //        wavy.image = UIImage(named: "nycSeaLevel1")
         
         return wavy
     }()
     
-//    public lazy var imageView: UIImageView = {
-//        let iv = UIImageView()
-//        iv.image = UIImage(named: "nycSeaLevel1")
-//        iv.contentMode = .scaleToFill
-//        iv.clipsToBounds = true
+        public lazy var imageView: UIImageView = {
+            let iv = UIImageView()
+            iv.image = UIImage(named: "nycSeaLevel1")
+            iv.contentMode = .scaleToFill
+            iv.clipsToBounds = true
+    
+//            let bg = UIBezierPath(rect: CGRect(x: 0, y: iv.frame.height - 20, width: iv.frame.width, height: iv.frame.height))
+//            bg.fill()
 //
-//        let bg = UIBezierPath(rect: CGRect(x: 0, y: iv.frame.height - 20, width: iv.frame.width, height: iv.frame.height))
-//        bg.fill()
-//
-//        // Add the first sine wave filled with a very transparent white
-//        let top1: CGFloat = 17.0
-//        let wave1 = wavyPath(rect: CGRect(x: 0, y: top1, width: frame.width, height: frame.height - top1), periods: 1.1, amplitude: 08, start: 0.05)
-//        UIColor(white: 1.0, alpha: 0.8).set()
-//        wave1.fill()
-//        return iv
-//    }()
+//            // Add the first sine wave filled with a very transparent white
+//            let top1: CGFloat = 17.0
+//            let wave1 = wavyPath(rect: CGRect(x: 0, y: top1, width: frame.width, height: frame.height - top1), periods: 1.1, amplitude: 08, start: 0.05)
+//            UIColor(white: 1.0, alpha: 0.8).set()
+//            wave1.fill()
+            addWaveBackground(to: iv)
+            return iv
+        }()
     
     public lazy var theView: UIView = {
         let v = UIView()
@@ -143,10 +145,11 @@ class Wave: UIView {
     }
     
     private func commonInit() {
-//        setupTheView()
+        //        setupTheView()
+        
+                setupImageView()
         setupWaveView()
-//        setupImageView()
-//        setupWaveImageView()
+        //        setupWaveImageView()
     }
     
     private func setupTheView() {
@@ -167,7 +170,7 @@ class Wave: UIView {
         
         NSLayoutConstraint.activate([
             wView.centerXAnchor.constraint(equalTo: centerXAnchor),
-//            wView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            //            wView.centerYAnchor.constraint(equalTo: centerYAnchor),
             wView.topAnchor.constraint(equalTo: self.topAnchor),
             wView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1),
             wView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3)
@@ -175,29 +178,28 @@ class Wave: UIView {
     }
     
     private func setupWaveImageView() {
-            addSubview(wImageView)
-            wImageView.translatesAutoresizingMaskIntoConstraints = false
-            
+        imageView.addSubview(wImageView)
+        wImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            wImageView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+            wView.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
+            wImageView.widthAnchor.constraint(equalTo: imageView.widthAnchor),
+            wImageView.heightAnchor.constraint(equalTo: imageView.heightAnchor)
+        ])
+    }
+    
+        private func setupImageView() {
+            addSubview(imageView)
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+    
             NSLayoutConstraint.activate([
-                wImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-    //            wView.centerYAnchor.constraint(equalTo: centerYAnchor),
-                wImageView.topAnchor.constraint(equalTo: self.topAnchor),
-                wImageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1),
-                wImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3)
+                imageView.topAnchor.constraint(equalTo: self.topAnchor),
+                imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                imageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1),
+                imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.3)
             ])
         }
-    
-//    private func setupImageView() {
-//        wView.addSubview(imageView)
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([
-//            imageView.centerXAnchor.constraint(equalTo: wView.centerXAnchor),
-//            imageView.centerYAnchor.constraint(equalTo: wView.centerYAnchor),
-//            imageView.widthAnchor.constraint(equalTo: wView.widthAnchor),
-//            imageView.heightAnchor.constraint(equalTo: wView.heightAnchor)
-//        ])
-//    }
     
 }
 
@@ -206,17 +208,17 @@ class WavyImageView: UIImageView {
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         return nil // TODO
     }
-
+    
     override func draw(_ rect: CGRect) {
         // Fill the whole background with the darkest blue color
         let white: UIColor = .secondarySystemBackground
         white.set()
-//        let image = UIImage(named: "nycSeaLevel1")
-//        UIColor(red: 0.329, green: 0.218, blue: 0.875, alpha: 1).set()
+        //        let image = UIImage(named: "nycSeaLevel1")
+        //        UIColor(red: 0.329, green: 0.218, blue: 0.875, alpha: 1).set()
         let bg = UIBezierPath(rect: rect)
         bg.fill()
         
@@ -225,25 +227,25 @@ class WavyImageView: UIImageView {
         let wave1 = wavyPath(rect: CGRect(x: 0, y: top1, width: frame.width, height: frame.height - top1), periods: 1.1, amplitude: 08, start: 0.05)
         let clear: UIColor = .systemTeal
         clear.set()
-//        UIColor(white: 1, alpha: 0.8).set()
+        //        UIColor(white: 1, alpha: 0.8).set()
         wave1.fill()
         
         
-
+        
         // Add the second sine wave over the first
-//        let top2: CGFloat = 34.0
-//        let wave2 = wavyPath(rect: CGRect(x: 0, y: top2, width: frame.width, height: frame.height - top2), periods: 1.5, amplitude: 21, start: 0.9)
-//        UIColor(white: 1.0, alpha: 0.15).set()
-//        wave2.fill()
-
+        //        let top2: CGFloat = 34.0
+        //        let wave2 = wavyPath(rect: CGRect(x: 0, y: top2, width: frame.width, height: frame.height - top2), periods: 1.5, amplitude: 21, start: 0.9)
+        //        UIColor(white: 1.0, alpha: 0.15).set()
+        //        wave2.fill()
+        
         // Add the text
-//        let paraAttrs = NSMutableParagraphStyle()
-//        paraAttrs.alignment = .center
-//        let textRect = CGRect(x: 0, y: frame.maxY - 64, width: frame.width, height: 24)
-//        let textAttrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.9), NSAttributedString.Key.paragraphStyle: paraAttrs]
-//        ("New user? Register here." as NSString).draw(in: textRect, withAttributes: textAttrs)
+        //        let paraAttrs = NSMutableParagraphStyle()
+        //        paraAttrs.alignment = .center
+        //        let textRect = CGRect(x: 0, y: frame.maxY - 64, width: frame.width, height: 24)
+        //        let textAttrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.9), NSAttributedString.Key.paragraphStyle: paraAttrs]
+        //        ("New user? Register here." as NSString).draw(in: textRect, withAttributes: textAttrs)
     }
-
+    
     // This creates the desired sine wave bezier path
     // rect is the area to fill with the sine wave
     // periods is how may sine waves fit across the width of the frame
@@ -251,10 +253,10 @@ class WavyImageView: UIImageView {
     // start is an offset in wavelengths for the left side of the sine wave
     func wavyPath(rect: CGRect, periods: Double, amplitude: Double, start: Double) -> UIBezierPath {
         let path = UIBezierPath()
-
+        
         // start in the bottom left corner
         path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-
+        
         let radsPerPoint = Double(rect.width) / periods / 2.0 / Double.pi
         let radOffset = start * 2 * Double.pi
         let xOffset = Double(rect.minX)
@@ -264,16 +266,16 @@ class WavyImageView: UIImageView {
         for x in stride(from: 0, to: Double(rect.width), by: 6) {
             let rad = Double(x) / radsPerPoint + radOffset
             let y = sin(rad) * amplitude
-
+            
             path.addLine(to: CGPoint(x: x + xOffset, y: y + yOffset))
         }
-
+        
         // Add the last point on the sine wave at the right edge
         let rad = Double(rect.width) / radsPerPoint + radOffset
         let y = sin(rad) * amplitude
-
+        
         path.addLine(to: CGPoint(x: Double(rect.maxX), y: y + yOffset))
-
+        
         // Add line from the end of the sine wave to the bottom right corner
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         // Close the path
@@ -281,4 +283,30 @@ class WavyImageView: UIImageView {
         
         return path
     }
+}
+
+func addWaveBackground(to view: UIImageView){
+    let leftDrop:CGFloat = 0.4
+    let rightDrop: CGFloat = 0.3
+    let leftInflexionX: CGFloat = 0.4
+    let leftInflexionY: CGFloat = 0.47
+    let rightInflexionX: CGFloat = 0.6
+    let rightInflexionY: CGFloat = 0.22
+    
+    let backView = UIImageView(frame: view.frame)
+    backView.backgroundColor = .gray
+    view.addSubview(backView)
+    let backLayer = CAShapeLayer()
+    let path = UIBezierPath()
+    path.move(to: CGPoint(x: 0, y: 0))
+    path.addLine(to: CGPoint(x:0, y: view.frame.height * leftDrop))
+    path.addCurve(to: CGPoint(x:view.frame.width, y: view.frame.height * rightDrop),
+                  controlPoint1: CGPoint(x: view.frame.width * leftInflexionX, y: view.frame.height * leftInflexionY),
+                  controlPoint2: CGPoint(x: view.frame.width * rightInflexionX, y: view.frame.height * rightInflexionY))
+    path.addLine(to: CGPoint(x:view.frame.width, y: 0))
+    path.close()
+    backLayer.fillColor = UIColor.blue.cgColor
+    backLayer.path = path.cgPath
+    backView.image = UIImage(named: "nycSeaLevel1")
+    backView.layer.addSublayer(backLayer)
 }
